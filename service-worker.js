2765173439e0a0b3fb9026569566859e95f587ba
sridhar96-alt <onigirizoro96@gmail.com>
2026-02-25 16:49:22 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,28 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("healify").then(cache=>cache.addAll(["./"]))
+const CACHE_NAME = "healify-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch from cache first
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
